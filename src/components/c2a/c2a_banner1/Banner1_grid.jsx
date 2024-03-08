49f4ea1d22ae304a0e999e69bdbbb3584837db16
Cap0n3 <cap0n3@protonmail.com>
2024-03-08 20:06:27 +0100

    Finished c2a banner with FX but no Ref for scrolling

--- src/components/c2a/c2a_banner1/Banner1_grid.jsx
+++ src/components/c2a/c2a_banner1/Banner1_grid.jsx
@@ -1,5 +1,8 @@
 import Grid from '@mui/material/Unstable_Grid2';
 import EmojiObjectsOutlinedIcon from '@mui/icons-material/EmojiObjectsOutlined';
+import MusicNoteOutlinedIcon from '@mui/icons-material/MusicNoteOutlined';
+import ContactSupportOutlinedIcon from '@mui/icons-material/ContactSupportOutlined';
+import CloudOutlinedIcon from '@mui/icons-material/CloudOutlined';
 import Typography from '@mui/material/Typography';
 
 const Banner1_grid = () => {
@@ -9,7 +12,7 @@ const Banner1_grid = () => {
                 <Grid xs={12} md={6} sx={{ 
                         display: 'flex',
                         flexDirection: 'column',
-                        justifyContent: 'center',
+                        justifyContent: 'flex-start',
                         alignItems: 'center',
                         gap: '1rem',
                     }}
@@ -17,77 +20,77 @@ const Banner1_grid = () => {
                     <EmojiObjectsOutlinedIcon sx={{ 
                             fontSize: 40, 
                             stroke: "red",
-                            strokeWidth: 0.5 
+                            strokeWidth: 0.5, 
                         }} 
                     />
-                    <Typography component="h3" variant="h6" color="secondary">
+                    <Typography component="h3" variant="h6" color="secondary" align="center">
                         Expertise
                     </Typography>
-                    <Typography variant="body2" color="secondary" fontWeight="fontWeightLight">
-                        Cours de guitare donné par un musicien professionnel avec plus de 20 ans d'expérience de la scène et du studio et de l'enseignement.
+                    <Typography variant="body2" color="secondary" fontWeight="fontWeightLight" align="center">
+                        Cours de guitare donné par un professeur avec plus de 20 ans d'expérience de la scène, du studio et de l'enseignement.
                     </Typography>
                 </Grid>
                 <Grid xs={12} md={6} sx={{ 
                         display: 'flex',
                         flexDirection: 'column',
-                        justifyContent: 'center',
+                        justifyContent: 'flex-start',
                         alignItems: 'center',
                         gap: '1rem',
                     }}
                 >
-                    <EmojiObjectsOutlinedIcon sx={{ 
+                    <MusicNoteOutlinedIcon sx={{ 
                             fontSize: 40, 
                             stroke: "red",
-                            strokeWidth: 0.5 
+                            strokeWidth: 0.5,  
                         }} 
                     />
-                    <Typography component="h3" variant="h6" color="secondary">
-                        Expertise
+                    <Typography component="h3" variant="h6" color="secondary" align="center">
+                        Expérience
                     </Typography>
-                    <Typography variant="body2" color="secondary" fontWeight="fontWeightLight">
-                        Cours de guitare donné par un musicien professionnel avec plus de 20 ans d'expérience de la scène et du studio et de l'enseignement.
+                    <Typography variant="body2" color="secondary" fontWeight="fontWeightLight" align="center">
+                        Expérience de la scène, du studio et de l'enseignement. MAO (Musique Assistée par Ordinateur) et enregistrement.
                     </Typography>
                 </Grid>
                 <Grid xs={12} md={6} sx={{ 
                         display: 'flex',
                         flexDirection: 'column',
-                        justifyContent: 'center',
+                        justifyContent: 'flex-start',
                         alignItems: 'center',
                         gap: '1rem',
                     }}
                 >
-                    <EmojiObjectsOutlinedIcon sx={{ 
+                    <ContactSupportOutlinedIcon sx={{ 
                             fontSize: 40, 
                             stroke: "red",
                             strokeWidth: 0.5 
                         }} 
                     />
-                    <Typography component="h3" variant="h6" color="secondary">
-                        Expertise
+                    <Typography component="h3" variant="h6" color="secondary" align="center">
+                        Support pédagogique
                     </Typography>
-                    <Typography variant="body2" color="secondary" fontWeight="fontWeightLight">
-                        Cours de guitare donné par un musicien professionnel avec plus de 20 ans d'expérience de la scène et du studio et de l'enseignement.
+                    <Typography variant="body2" color="secondary" fontWeight="fontWeightLight" align="center">
+                        Support pédagogique de qualité pour un apprentissage rapide et efficace.
                     </Typography>
                 </Grid>
                 <Grid xs={12} md={6} sx={{ 
                         display: 'flex',
                         flexDirection: 'column',
-                        justifyContent: 'center',
+                        justifyContent: 'flex-start',
                         alignItems: 'center',
                         gap: '1rem',
                     }}
                 >
-                    <EmojiObjectsOutlinedIcon sx={{ 
+                    <CloudOutlinedIcon sx={{ 
                             fontSize: 40, 
                             stroke: "red",
                             strokeWidth: 0.5 
                         }} 
                     />
-                    <Typography component="h3" variant="h6" color="secondary">
-                        Expertise
+                    <Typography component="h3" variant="h6" color="secondary" align="center">
+                        Cloud
                     </Typography>
-                    <Typography variant="body2" color="secondary" fontWeight="fontWeightLight">
-                        Cours de guitare donné par un musicien professionnel avec plus de 20 ans d'expérience de la scène et du studio et de l'enseignement.
+                    <Typography variant="body2" color="secondary" fontWeight="fontWeightLight" align="center">
+                        Cours et support pédagogique disponibles sur le cloud à tout moment.
                     </Typography>
                 </Grid>
             </Grid>
--- src/components/c2a/c2a_banner1/C2A_Banner1.jsx
+++ src/components/c2a/c2a_banner1/C2A_Banner1.jsx
@@ -1,9 +1,12 @@
+import { useEffect, useState, useRef } from 'react';
 import { Typography } from '@mui/material';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Unstable_Grid2';
 import Al_Detour from "../../../assets/img/Al1_Website24_detour.png";
 import styled from 'styled-components';
 import Banner1_grid from './Banner1_grid';
+import Slide from '@mui/material/Slide';
+
 
 const Al_Image = styled('img')(
     ({theme}) => `
@@ -11,44 +14,85 @@ const Al_Image = styled('img')(
         height: auto;
         max-width: 100%;
         display: block;
-        margin-left: auto;
-        margin-right: auto;
     ` 
 );
 
 const C2A_Banner1 = () => {
+    const [isVisible, setIsVisible] = useState(false);
+    const leftCell_container  = useRef(null);
+    const middleCell_container  = useRef(null);
+    const rightCell_container  = useRef(null);
+
+    useEffect(() => {
+        setIsVisible(true);
+
+        return () => {
+            setIsVisible(false);
+        }
+    }
+    , []);
+
     return (
         <Box sx={{
             flexGrow: 1,
             pt: '2rem',
             pl: '2rem',
             pr: '2rem',
-            border: '1px solid red',
         }}>
             <Grid container spacing={2}>
-                <Grid xs={12} md={4} sx={{
+                <Grid 
+                    xs={12} 
+                    md={4} 
+                    sx={{
                         display: 'flex',
                         flexDirection: 'column',
                         justifyContent: 'center',
                         gap: '1rem',
+                        overflow: 'hidden',
+                    }}
+                    ref={leftCell_container}
+                >
+                    <Slide direction="down" in={isVisible} container={leftCell_container.current} style={{ transitionDelay: isVisible ? '1000ms' : '0ms' }}>
+                        <Box>
+                            <Typography component="h2" variant="h5" color="secondary" gutterBottom>
+                                Cours de guitare depuis 2010.
+                            </Typography>
+                            <Typography variant="body2" color="secondary" fontWeight="fontWeightLight">
+                                Cours de guitare donné par un Alex, un musicien professionnel avec plus de 20 ans d'expérience de la scène et du studio et de l'enseignement.
+                            </Typography>
+                        </Box>
+                    </Slide>
+                </Grid>
+                <Grid xs={6} md={4} ref={middleCell_container} sx={{ 
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'flex-end',
+                        justifyContent: 'flex-end',
+                        overflow: 'hidden',
                     }}
                 >
-                    <Typography component="h2" variant="h5" color="secondary" sx={{
-                                      
-                        }} 
-                    gutterBottom>
-                        Cours de guitare depuis 2010.
-                    </Typography>
-                    <Typography variant="body2" color="secondary" fontWeight="fontWeightLight">
-                        Cours de guitare donné par un Alex, un musicien professionnel avec plus de 20 ans d'expérience de la scène et du studio et de l'enseignement.
-
-                    </Typography>
+                    <Slide direction="up" in={isVisible} container={middleCell_container.current} style={{ transitionDelay: isVisible ? '1000ms' : '0ms' }}>
+                        <Al_Image src={Al_Detour} alt="Alex" />
+                    </Slide>
                 </Grid>
-                <Grid xs={6} md={4}>
-                    <Al_Image src={Al_Detour} alt="Alex" />
-                </Grid>
-                <Grid xs={6} md={4}>
-                    <Banner1_grid />
+                <Grid 
+                    xs={6} 
+                    md={4} 
+                    ref={rightCell_container}
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        gap: '1rem',
+                        overflow: 'hidden',
+                    }} 
+                >
+                    <Slide direction="down" in={isVisible} container={rightCell_container.current} style={{ transitionDelay: isVisible ? '1000ms' : '0ms' }}>
+                        <Box>
+                            <Banner1_grid />
+                        </Box>
+                    </Slide>
                 </Grid>
             </Grid>
         </Box>

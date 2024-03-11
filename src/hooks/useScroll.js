08283379cb7ba6fd57fb4bd2cb74e06e11d37ac9
Cap0n3 <cap0n3@protonmail.com>
2024-03-11 08:37:06 +0100

    Added useAppear hook file but without using it

---
+++ src/hooks/useScroll.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+
+/**
+ * Hook to get current scroll position of page.
+ *  
+ * @returns     Float that represents current position. 
+ */
+const useScroll = () => {
+    const [scrollPosition, setScrollPosition] = useState(0);
+
+    const handleScroll = () => {
+        const position = window.pageYOffset || document.documentElement.scrollTop;;
+        setScrollPosition(position);
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll, { passive: true });
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    return scrollPosition;
+}
+
+export default useScroll;

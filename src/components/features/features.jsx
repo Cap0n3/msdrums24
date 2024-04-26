2fa40c5b8af8e6d7bff601ae66bc852c0faee2e9
Cap0n3 <cap0n3@protonmail.com>
2024-04-26 13:26:57 +0200

    Added basic fx to feature section

--- src/components/features/features.jsx
+++ src/components/features/features.jsx
@@ -4,6 +4,9 @@ import { Container, Box, Typography, Paper } from '@mui/material';
 import BusinessIcon from '@mui/icons-material/Business';
 import WorkHistoryIcon from '@mui/icons-material/WorkHistory';
 import PhotoCameraFrontIcon from '@mui/icons-material/PhotoCameraFront';
+import Fade from '@mui/material/Fade';
+import Slide from '@mui/material/Slide';
+import { useInView } from 'react-intersection-observer';
 
 
 const features = [
@@ -27,6 +30,18 @@ const features = [
 const Features = () => {
     const theme = useTheme();
 
+    const [refTitleSlide, inViewTitle] = useInView({
+        triggerOnce: true,
+        threshold: 1, 
+        delay: 1000
+    });
+    
+    const [refFeatSlide, inViewFeat] = useInView({
+        triggerOnce: true,
+        threshold: 1, 
+        delay: 1000
+    });
+
     const featureLogo = (IconComponent, iconProps) => {
         return (
             <Box display="flex" justifyContent="center">
@@ -36,43 +51,58 @@ const Features = () => {
     }
 
     return (
-        <Box py={4} sx={{ backgroundColor: theme.palette.background.default }}>
-            <Typography variant="h4" align="center" fontWeight={400} sx={{ color: theme.palette.text.primary }} gutterBottom>
-                Say Hello to great features !
-            </Typography>
-            <Typography variant="h6" align="center" mb={4} sx={{ color: theme.palette.text.secondary }}>
-                Here are some of the features that you'll love.
-            </Typography>
-            <Box display="flex" justifyContent="space-around" my={4} sx={{ flexWrap: 'wrap', gap: 2, maxWidth: 1200, margin: 'auto' }}>
-                {features.map((feature, index) => (
-                    <Paper 
-                        key={index} 
-                        elevation={1} 
-                        square={false} 
-                        sx={{ 
-                            m: 2, 
-                            p: 4, 
-                            textAlign: 'center', 
-                            maxWidth: 340,
-                            backgroundColor: theme.palette.background.default
-                        }}
-                    >
-                        {featureLogo(
-                            feature.logo, 
-                            { 
-                                fontSize:"large",
-                                sx: { color: theme.palette.primary.main }
-                            })
-                        }
-                        <Typography variant="h6" mt={2} fontWeight={600} sx={{ color: theme.palette.text.primary }}>
-                            {feature.title}
-                        </Typography>
-                        <Typography variant="subtitle1" mt={2} sx={{ color: theme.palette.text.secondary }}>
-                            {feature.text}
-                        </Typography>
-                    </Paper>
-                ))}
-            </Box>
+        <Box py={4} sx={{ backgroundColor: theme.palette.background.default, overflow: 'hidden' }}>
+            <Slide direction="up" in={inViewTitle} timeout={1200}>
+                <Container ref={refTitleSlide}>
+                    <Typography variant="h4" align="center" fontWeight={400} sx={{ color: theme.palette.text.primary }} gutterBottom>
+                        Say Hello to great features !
+                    </Typography>
+                    <Typography variant="h6" align="center" mb={4} sx={{ color: theme.palette.text.secondary }}>
+                        Here are some of the features that you'll love.
+                    </Typography>
+                </Container>
+            </Slide>
+            
+            <Slide direction="up" in={inViewFeat} timeout={1500}>
+                <Box ref={refFeatSlide} my={4} sx={{ 
+                        display: 'flex',
+                        justifyContent: 'space-around',
+                        flexWrap: 'wrap', 
+                        gap: 2, 
+                        maxWidth: 1200, 
+                        margin: 'auto',
+                    }}
+                >
+                    {features.map((feature, index) => (
+                        <Paper 
+                            key={index} 
+                            elevation={1} 
+                            square={false} 
+                            sx={{ 
+                                m: 2, 
+                                p: 4, 
+                                textAlign: 'center', 
+                                maxWidth: 340,
+                                backgroundColor: theme.palette.background.default
+                            }}
+                        >
+                            {featureLogo(
+                                feature.logo, 
+                                { 
+                                    fontSize:"large",
+                                    sx: { color: theme.palette.primary.main }
+                                })
+                            }
+                            <Typography variant="h6" mt={2} fontWeight={600} sx={{ color: theme.palette.text.primary }}>
+                                {feature.title}
+                            </Typography>
+                            <Typography variant="subtitle1" mt={2} sx={{ color: theme.palette.text.secondary }}>
+                                {feature.text}
+                            </Typography>
+                        </Paper>
+                    ))}
+                </Box>
+            </Slide>
         </Box>
     );
 };
